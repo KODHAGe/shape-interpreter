5a9e788090c1cfd3a4263456d47cf4b9b2e7ead5
Wolf <wolf.wikgren@gmail.com>
2018-11-21 13:21:38 +0200

    Add authentication

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
+require('dotenv').config()
+const jwt = require('jwt-simple')
+const secret = process.env.JWT_SECRET
+
 // Service/server
 const { send } = require('micro')
 const { router, get, post } = require('microrouter')
@@ -23,36 +27,58 @@ async function get_latest_model() {
   }
 }
 
+const authenticate = async (req, res, callback) => {
+  let body = await json(req)
+  let token = body.jwt
+  try {
+    let decoded = jwt.decode(token, secret)
+    if (decoded) {
+      res.setHeader('Access-Control-Allow-Origin', '*')
+      // Add specific authentication scope handling here
+      return callback(req, res)
+    }
+  } catch (err) {
+    send(res, 401, 'Authentication failed: ' + err.message)
+    return false
+  }
+}
+
 async function update_model(req, res) {
-  modeler.update()
-  let latest = get_latest_model()
-  send(res, 200, 'Training the model. This will take some time. Current version is '+ latest + '.')
+  await authenticate(req, res, async() => {
+    modeler.update()
+    let latest = get_latest_model()
+    send(res, 200, 'Training the model. This will take some time. Current version is '+ latest + '.')  
+  })
 }
 
 async function get_model(req, res) {
-  let latest = await get_latest_model()
-  const model = await tf.loadModel('file://model/' + latest + '/model.json')
-  send(res, 200, model)
+  await authenticate(req, res, async() => {
+    let latest = await get_latest_model()
+    const model = await tf.loadModel('file://model/' + latest + '/model.json')
+    send(res, 200, model)
+  })
 }
 
 async function make_prediction (req, res) {
-  // default to zeroes
-  let array = [0,0,0,0,0,0,0,0,0]
-  if(req.params.array) {
-    array = JSON.parse(req.params.array)
-    console.log(typeof(JSON.parse(req.params.array)))
-  }
-  let latest = await get_latest_model()
-  const model = await tf.loadModel('file://model/' + latest + '/model.json')
-  let prediction = await model.predictOnBatch(tf.tensor2d(array, [1,9]))
-  let values = prediction.dataSync()
-  let order = modeler.config.firestore.sort_order
-  let predictionResultObject = {}
-  for (let i = 0; i < order.length; i++) {
-    predictionResultObject[order[i]] = values[i]
-  }
-  predictionResultObject['modelVersion'] = latest
-  send(res, 200, predictionResultObject)
+  await authenticate(req, res, async() => {
+    // default to zeroes
+    let array = [0,0,0,0,0,0,0,0,0]
+    if(req.params.array) {
+      array = JSON.parse(req.params.array)
+      console.log(typeof(JSON.parse(req.params.array)))
+    }
+    let latest = await get_latest_model()
+    const model = await tf.loadModel('file://model/' + latest + '/model.json')
+    let prediction = await model.predictOnBatch(tf.tensor2d(array, [1,9]))
+    let values = prediction.dataSync()
+    let order = modeler.config.firestore.sort_order
+    let predictionResultObject = {}
+    for (let i = 0; i < order.length; i++) {
+      predictionResultObject[order[i]] = values[i]
+    }
+    predictionResultObject['modelVersion'] = latest
+    send(res, 200, predictionResultObject)
+  })
 }
 
 module.exports = router(
